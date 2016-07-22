376128b2af6c1f9659ffb2ccad0d2361416e11ec
jolivares <jolivares@JOLIVARES>
2016-07-22 15:39:11 -0500

    method and class decorators examples

---
+++ org.company.order.web/wwwroot/decorators/loader-decorator.js
@@ -0,0 +1,40 @@
+﻿export function logger(message)
+{
+    return function(target, name, descriptor)
+    {
+        let fn = descriptor.value;
+
+        let newFn  = function() {
+            console.log('starting %s', name);
+            console.log(message || "no message");
+            fn.apply(target, arguments);
+            console.log('ending %s', name);
+           
+            r(function(){
+                console.log("all the content loaded")
+            });
+
+            function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
+
+        };
+
+        descriptor.value = newFn;
+
+        return descriptor;
+    }
+}
+
+export function loader(isActivate)
+{
+    return function(target){
+        let loader =isActivate ? function(){
+            console.log("loading...");
+
+            r(function(){
+                console.log("done!")
+            });
+
+            function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
+        }() : '';
+    }
+}
--- org.company.order.web/wwwroot/home/index.js
+++ org.company.order.web/wwwroot/home/index.js
@@ -1,2 +1,17 @@
-﻿export class Index
-{}
+﻿import {logger, loader} from 'decorators/loader-decorator'
+
+
+@loader(true)
+export class Index
+{
+    constructor(){
+        this.someMethod()
+    }
+
+    @logger("custom message")
+    someMethod(){
+        console.log("inside some method")
+    }
+
+}
+
