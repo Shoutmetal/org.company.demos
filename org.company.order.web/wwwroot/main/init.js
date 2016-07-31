50041c07f108bdc21d45bb5b59b180a18acc21a6
Jose Olivares <shoutmetal@gmail.com>
2016-07-31 12:31:28 -0500

    sessionStorage on minimize/maximize added refactor init mechanism

--- org.company.order.web/wwwroot/main/init.js
+++ org.company.order.web/wwwroot/main/init.js
@@ -1,17 +1,7 @@
-﻿import {inject} from 'aurelia-framework';  
-
-@inject(Element)
-export class Menu
+﻿export class Menu
 {
-    constructor(element){
-        this.element = element;
-        this.current = null;
-        this.selected = null;
-        this.expanded = true;
-    }
 
     configureRouter(config, router){
-        this.router = router;
         config.title= "Demo Architecture";
 
         let reports = [
@@ -28,115 +18,14 @@ export class Menu
         routes = routes.concat(reports);
         config.map(routes);
 
+        this.router = router;
+
         this.router.navigateWithParams = (routeName, params) => {
             let routerParams = this.router.routes.find(x => x.name === routeName);
             routerParams.data = params;
             this.router.navigate(routeName);
         }
-    }
-
-    attached(){
-        let current = this.router.navigation.filter( item => item.isActive)[0];
-
-        if(current.config.parentName){
-            this.current = $(this.element).find("#" + current.config.parentName)
-            this.current.addClass("start active open");
-            this.current.find("span.arrow").addClass("open");
-        }else{
-            this.current = $(this.element).find("#" + current.config.name)
-            this.current.addClass("start active");
-        }
-
-
-        if ($.cookie && $.cookie('sidebar_closed') === '1' && Metronic.getViewPort().width >= resBreakpointMd) {
-            $('body').addClass('page-sidebar-closed');
-            $('.page-sidebar-menu').addClass('page-sidebar-menu-closed');
-        }
-    }
-
-    open(route){
-
-        this.selected = this.selected || this.current;
-        this.current = $(this.element).find("#" + route.config.name);
-
-
-        if(route.config.hasChilds){
-
-            let isOpen = this.current.hasClass("open");
-            
-            $(".page-sidebar-menu li").each(function(){
-                $(this).removeClass("open") 
-                $(this).find("ul").hide();
-                $(this).find("span.arrow").removeClass("open");
-            })
-
-            if(isOpen) {
-                this.current.removeClass("open") 
-                this.current.find("ul").hide();
-                this.current.find("span.arrow").removeClass("open");
-            }
-            else {
-                this.current.addClass("open") 
-                this.current.find("ul").show();
-                this.current.find("span.arrow").addClass("open");
-            }
-
-            return true;
-        }
-
-        if((this.selected).is(this.current)) return true;
-
-        this.selected.removeClass("start active open");
-        this.selected.find(".arrow").removeClass("open");
-        this.selected.find("ul").hide();
-        this.current.addClass("start active");
-        this.selected = this.current;
-
-        return true;
 
     }
 
-    openSub(route){
-        this.selected.removeClass("start active open");
-        this.selected.find(".arrow").removeClass("open");
-        this.selected.find("ul").hide();
-
-        this.selected = $(this.element).find("#" + route.config.name);
-        this.selected.addClass("start active open");
-
-        return true;
-    }
-
-    sliderToggle(){
-        var body = $('body');
-        var sidebar = $('.page-sidebar');
-        var sidebarMenu = $('.page-sidebar-menu');
-        $(".sidebar-search", sidebar).removeClass("open");
-
-        if (body.hasClass("page-sidebar-closed")) {
-            body.removeClass("page-sidebar-closed");
-            sidebarMenu.removeClass("page-sidebar-menu-closed");
-            if ($.cookie) {
-                $.cookie('sidebar_closed', '0');
-            }
-        } else {
-            body.addClass("page-sidebar-closed");
-            sidebarMenu.addClass("page-sidebar-menu-closed");
-            if (body.hasClass("page-sidebar-fixed")) {
-                sidebarMenu.trigger("mouseleave");
-            }
-            if ($.cookie) {
-                $.cookie('sidebar_closed', '1');
-            }
-        }
-
-        $(window).trigger('resize');
-    }
-
-    getRoute(name){
-        let current = this.router.navigation.filter( item => item.config.name == name)[0];
-
-        return current;
-    }
-    
 }
---
+++ org.company.order.web/wwwroot/main/navbar.js
@@ -0,0 +1,141 @@
+﻿import {inject} from 'aurelia-framework';  
+import {EventAggregator} from 'aurelia-event-aggregator';
+
+@inject(Element, EventAggregator)
+export class NavBar
+{
+    constructor(element, event){
+        this.element = element;
+        this.event = event;
+        this.current = null;
+        this.selected = null;
+        this.expanded = true;
+        this.route = null;
+    }
+
+    activate(router){
+        this.router = router;
+    }
+
+    attached(){
+        this.defaultRoute();
+        this.cookieMenu();
+
+        this.event.subscribe("router:navigation:success", (result) => {
+
+            if(this.route && !this.route.config.parentName){
+                let route = this.route
+
+                this.selected = this.selected || this.current;
+                this.current = $(this.element).find("#" + route.config.name);
+
+                if((this.selected).is(this.current)) return true;
+
+                this.selected.removeClass("start active open");
+                this.selected.find(".arrow").removeClass("open");
+                this.selected.find("ul").hide();
+                this.current.addClass("start active");
+                this.selected = this.current;
+            }
+
+        }); 
+    }
+
+    defaultRoute(){
+        let current = this.router.navigation.filter( item => item.isActive)[0];
+
+        if(current.config.parentName){
+            this.current = $(this.element).find("#" + current.config.parentName)
+            this.current.addClass("start active open");
+            this.current.find("span.arrow").addClass("open");
+        }else{
+            this.current = $(this.element).find("#" + current.config.name)
+            this.current.addClass("start active");
+        }
+    }
+
+    cookieMenu(){
+
+        let isClosed = sessionStorage.getItem("sidebar_closed")
+
+        if (isClosed === '1' ) {
+            $('body').addClass('page-sidebar-closed');
+            $('.page-sidebar-menu').addClass('page-sidebar-menu-closed');
+        }
+    }
+
+    open(route){
+        this.route = route;
+
+        if(route.config.hasChilds){
+
+            this.selected = this.selected || this.current;
+            this.current = $(this.element).find("#" + route.config.name);
+
+            let isOpen = this.current.hasClass("open");
+            
+            $(".page-sidebar-menu li").each(function(){
+                $(this).removeClass("open") 
+                $(this).find("ul").hide();
+                $(this).find("span.arrow").removeClass("open");
+            })
+
+            if(isOpen) {
+                this.current.removeClass("open") 
+                this.current.find("ul").hide();
+                this.current.find("span.arrow").removeClass("open");
+            }
+            else {
+                this.current.addClass("open") 
+                this.current.find("ul").show();
+                this.current.find("span.arrow").addClass("open");
+            }
+
+        }
+
+        return true;
+    }
+
+    openSub(route){
+        this.route = route;
+
+        if((this.selected).is(this.current)) return true;
+
+        this.selected.removeClass("start active open");
+        this.selected.find(".arrow").removeClass("open");
+        this.selected.find("ul").hide();
+
+        this.selected = $(this.element).find("#" + route.config.parentName);
+        this.selected.addClass("start active open");
+
+        return true;
+    }
+
+    sliderToggle(){
+        var body = $('body');
+        var sidebar = $('.page-sidebar');
+        var sidebarMenu = $('.page-sidebar-menu');
+        $(".sidebar-search", sidebar).removeClass("open");
+
+        if (body.hasClass("page-sidebar-closed")) {
+            body.removeClass("page-sidebar-closed");
+            sidebarMenu.removeClass("page-sidebar-menu-closed");
+            sessionStorage.setItem("sidebar_closed", "0")
+        } else {
+            body.addClass("page-sidebar-closed");
+            sidebarMenu.addClass("page-sidebar-menu-closed");
+            if (body.hasClass("page-sidebar-fixed")) {
+                sidebarMenu.trigger("mouseleave");
+            }
+            sessionStorage.setItem("sidebar_closed", "1")
+        }
+
+        $(window).trigger('resize');
+    }
+
+    getRoute(name){
+        let current = this.router.navigation.filter( item => item.config.name == name)[0];
+
+        return current;
+    }
+}
