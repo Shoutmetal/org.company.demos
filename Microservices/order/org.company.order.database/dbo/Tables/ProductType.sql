7247f8f3ce5b1951d80021b9c861a708504159dd
jolivares <jolivares@JOLIVARES>
2018-03-22 10:58:43 -0500

    database scripts

--- Microservices/order/org.company.order.database/dbo/Tables/ProductType.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[ProductType] (
-    [ProductTypeId] INT           IDENTITY (1, 1) NOT NULL,
-    [Name]          VARCHAR (500) NOT NULL,
-    [Active]        BIT           NOT NULL,
-    PRIMARY KEY CLUSTERED ([ProductTypeId] ASC)
-);
-
-
-
--- Microservices/order/org.company.order.database/dbo/Tables/Status.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Status] (
-    [StatusId] INT           IDENTITY (1, 1) NOT NULL,
-    [Name]     VARCHAR (100) NULL,
-    [Active]   BIT           NULL,
-    PRIMARY KEY CLUSTERED ([StatusId] ASC)
-);
-
